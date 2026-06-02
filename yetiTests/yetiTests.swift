ff911093327056e398a2677914e0cdc247d9d9a3
krytec_gaming <kehoe.brogna@gmail.com>
2026-06-02 17:51:34 -0600

    Initial Commit

---
+++ yetiTests/yetiTests.swift
@@ -0,0 +1,17 @@
+//
+//  yetiTests.swift
+//  yetiTests
+//
+//  Created by Home on 6/2/26.
+//
+
+import Testing
+@testable import yeti
+
+struct yetiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
